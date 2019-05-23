63182eb17fc85f615ae301f3ac69e66dcd30310d
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-05-23 19:37:45 -0400

    boop

--- frontend/src/components/AccountEditing.js
+++ frontend/src/components/AccountEditing.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { NotificationManager } from "react-notifications";
-
 import { URL } from "../helpers";
 
 class AccountEditing extends Component {
