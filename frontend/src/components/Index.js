4b6e0566e4fe25231609f03bcbd5e5c7bea03ea3
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-07-11 22:29:25 -0400

    adding basic text so main page isn't just empty lol

--- frontend/src/components/Index.js
+++ frontend/src/components/Index.js
@@ -3,6 +3,10 @@ import React from "react";
 const Index = props => (
   <div className="container">
     <h2>Home</h2>
+    <h5 className="welcomeText">
+      Learn how to throw every grenade on all competetive maps.
+    </h5>
+    <p className="subText">Create an account to do x, y, and z.</p>
   </div>
 );
 
