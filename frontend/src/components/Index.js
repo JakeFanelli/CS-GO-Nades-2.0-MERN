48e05416d0d63bde25cc8eea5fb0aeee122d700d
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-08-08 22:29:53 -0400

    picture not selectable, and updating text on home page. Really need a design for landing page....

--- frontend/src/components/Index.js
+++ frontend/src/components/Index.js
@@ -3,9 +3,12 @@ import React from "react";
 const Index = props => (
   <div className="container">
     <h5 className="welcomeText">
-      Learn how to throw every grenade on all competetive maps.
+      Learn how to throw every grenade on all competetive maps and recoil
+      patterns
     </h5>
-    <p className="subText">Create an account to do x, y, and z.</p>
+    <p className="subText">
+      Create an account to submit nades, and vote on user submitted nades
+    </p>
   </div>
 );
 
