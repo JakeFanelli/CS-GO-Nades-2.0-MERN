c6042beca91cdd04ab3ab734f0d0c9be80e56e16
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2019-09-03 00:03:25 -0400

    google analytics test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,12 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import "react-notifications/lib/notifications.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import ReactGA from "react-ga";
 
 library.add(fas);
+
+ReactGA.initialize("UA-129029364-2");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
   constructor() {
