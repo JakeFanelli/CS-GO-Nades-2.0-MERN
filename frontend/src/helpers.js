6d16c3c46ff69b43215bff81778087fa6d2b8483
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2023-10-30 18:35:20 -0400

    feat: trying out onrender site

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -2,4 +2,4 @@ export const regexHasLower = new RegExp("^(?=.*[a-z])");
 export const regexHasUpper = new RegExp("^(?=.*[A-Z])");
 export const regexHasNum = new RegExp("^(?=.*[0-9])");
 export const regexHasLength = new RegExp("^(?=.{8,})");
-export const URL = "https://csgo-nades.herokuapp.com";
+export const URL = "https://cs2-nades.onrender.com/";
